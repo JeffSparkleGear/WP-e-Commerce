70b5b3311b694ff4d252f6bff79439cf5a7762f4
SparkleGear <jeff@sparkle-gear.com>
2013-01-31 06:54:07 -0500

    As discussed in #153
    My suggestion is leave the 'wpsc_save_cart_item' action alone so we
    don't break anything ,deprecate it.
    
    Add a do_action('wpsc_before_save_cart_item', $cart_id, $this ) call
    before the cart item record and cart meta is stored into the DB.
    
    Add a do_action('wpsc_after_save_cart_item', $cart_id, $this ) call
    after the cart item record and cart meta is stored into the DB.

--- wpsc-includes/cart.class.php
+++ wpsc-includes/cart.class.php
@@ -2065,6 +2065,8 @@ class wpsc_cart_item {
          $tax_rate = $taxes['rate'];
          $tax = $taxes['tax'];
       }
+      
+      do_action('wpsc_before_save_cart_item', $cart_id, $this );
 
 $wpdb->insert(
 		WPSC_TABLE_CART_CONTENTS,
@@ -2147,6 +2149,9 @@ $wpdb->insert(
 
       }
 
+      do_action('wpsc_after_save_cart_item', $cart_id, $this );
+
+      // wpsc_save_cart_item should be deprecated in favor of wpsc_after_save_cart_item
       do_action('wpsc_save_cart_item', $cart_id, $this->product_id);
    }
 
