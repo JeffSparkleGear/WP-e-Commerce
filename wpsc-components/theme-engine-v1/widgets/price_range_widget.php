e0a6e4af2c4d130e85f07dc9814fc88d75c27065
jeff <jeff@pyebrook.com>
2015-08-23 14:44:55 -0400

    parent::__construct to avoid deprecated constructor warning

--- wpsc-components/theme-engine-v1/widgets/price_range_widget.php
+++ wpsc-components/theme-engine-v1/widgets/price_range_widget.php
@@ -19,7 +19,7 @@ class WP_Widget_Price_Range extends WP_Widget {
 			'description' => __( 'Price Range Widget', 'wpsc' )
 		);
 
-		$this->WP_Widget( 'wpsc_price_range', __( '(WPEC) Price Range', 'wpsc' ), $widget_ops );
+		parent::__construct( 'wpsc_price_range', __( '(WPEC) Price Range', 'wpsc' ), $widget_ops );
 
 	}
 
